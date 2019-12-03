b4143f7ad9310bbcbc285c98dd69668c2e7d9758
אביגיל פיטוסי <afitouss@acad.jct.ac.il>
2019-12-03 16:12:42 +0200

    first commit

---
+++ Csharp T2/GuestRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_T2
+{
+    class GuestRequest
+    {
+        public
+        DateTime EntryDate;
+        DateTime ReleaseDate;
+        bool IsApproved;
+        public override string ToString()
+        {
+            return EntryDate.ToString("DD/MM/YY") + " " + ReleaseDate.ToString("DD/MM/YY");
+        }
+
+    }
+}
---
+++ Csharp T2/HostingUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_T2
+{
+    class HostingUnit
+    {
+        private static int stSerialKey {};
+
+
+    }
+}
